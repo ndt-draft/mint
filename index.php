026bd8715f08849b29404b651866c697d3e6d75f
thanh4890 <thanh4890@gmail.com>
2015-07-28 17:02:24 +0700

    default layout for index

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php get_header(); ?>
 
-    <main class="content-area large-9 columns">
-        <?php if ( have_posts() ) {
-	        while ( have_posts() ) {
-	            the_post();
+<div class="row container template-default">
+    <div id="primary" class="content-area large-9 columns">
+        <main id="main" class="site-main">
+            <?php if ( have_posts() ) {
+                while ( have_posts() ) {
+                    the_post();
 
-                the_content();
-	        }
-	    } ?>
-    </main> <!-- .content-area -->
-    <div class="widget-area large-3 columns">
-        sidebar
-    </div> <!-- .widget-area -->
+                    the_content();
+                }
+            } ?>
+        </main> <!-- #main -->
+    </div> <!-- #primary -->
+
+    <div id="secondary" class="widget-area large-3 columns">
+        <p>widget area</p>
+    </div> <!-- #secondary -->
+</div> <!-- .row -->
 
 <?php get_footer(); ?>
